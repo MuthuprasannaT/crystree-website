0da656fbeabd10f59a592b30a7458642ab51f7c1
Gopala Krishnan T <gopalakrishnanhoneybei20@gmail.com>
2026-06-08 14:47:44 +0530

    Footer

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -2,6 +2,46 @@
 <html lang="en">
 
 <head>
+    <title>@yield('title', 'Crystree Solutions Pvt Ltd | Software Development Company in Trichy')</title>
+
+<meta name="description"
+    content="Crystree Solutions Pvt Ltd is a leading Software Development Company in Trichy offering Website Development, Mobile App Development, ERP Software, CRM Software, Digital Marketing, SEO Services and E-Commerce Solutions.">
+
+<meta name="keywords"
+    content="Software Development Company Trichy, Website Development Trichy, Mobile App Development, ERP Software, CRM Software, SEO Services, Digital Marketing, E-Commerce Solutions">
+
+<meta name="author" content="Crystree Solutions Pvt Ltd">
+
+<meta name="robots" content="index, follow">
+
+<meta property="og:title" content="Crystree Solutions Pvt Ltd">
+<meta property="og:description"
+    content="Software Development, Mobile App Development, ERP Solutions, Digital Marketing and SEO Services.">
+<meta property="og:image" content="{{ asset('assets/img/logo/newlogo1.png') }}">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:type" content="website">
+
+<link rel="canonical" href="{{ url()->current() }}">
+
+<script type="application/ld+json">
+{
+ "@context":"https://schema.org",
+ "@type":"Organization",
+ "name":"Crystree Solutions Pvt Ltd",
+ "url":"https://www.crystreesolutions.com",
+ "logo":"https://www.crystreesolutions.com/assets/img/logo/newlogo1.png",
+ "telephone":"+91-9566968212",
+ "email":"support@crystreesolutions.com",
+ "address":{
+ "@type":"PostalAddress",
+ "streetAddress":"F9 J.C. Abode Karur Bypass Road",
+ "addressLocality":"Tiruchirappalli",
+ "addressRegion":"Tamil Nadu",
+ "postalCode":"620018",
+ "addressCountry":"IN"
+ }
+}
+</script>
     @include('Layouts.header')
 </head>
 
