f82895594d99431b4a3e1a02aa95199c98fdd4af
Gopala Krishnan T <gopalakrishnanhoneybei20@gmail.com>
2026-07-20 13:46:20 +0530

    gopal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,24 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@crystree.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('CrystreeSolution'),
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@crystree.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('Admin_Crystree26'),
+            ]
+        );
     }
 }
